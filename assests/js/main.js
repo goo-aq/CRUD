da3e12d780e6e8f7e6af5f21663d68d4bccb61d2
goo-aq <120787130+goo-aq@users.noreply.github.com>
2023-01-02 14:14:49 +0200

    validation

--- assests/js/main.js
+++ assests/js/main.js
@@ -3,6 +3,17 @@ var courseCategory = document.getElementById("courseCategory");
 var coursePrice = document.getElementById("coursePrice");
 var courseDescription = document.getElementById("courseDescription");
 var courseCapacity = document.getElementById("courseCapacity");
+var nameAlert = document.getElementById("nameAlert");
+var categoryAlert = document.getElementById("categoryAlert");
+var priceAlert = document.getElementById("priceAlert");
+var descriptionAlert = document.getElementById("descriptionAlert");
+var capacityAlert = document.getElementById("capacityAlert");
+var nameFlag = 0;
+var categoryFlag = 1;
+var priceFlag = 1;
+var descriptionFlag = 1;
+var capacityFlag = 1;
+console.log(descriptionFlag);
 var addbtn = document.getElementById("add");
 var courses = [];
 var table = document.getElementById("table");
@@ -12,7 +23,7 @@ if (JSON.parse(localStorage.getItem("courses")) != null) {
   courses = JSON.parse(localStorage.getItem("courses"));
   display();
 }
-
+// add/update button
 addbtn.onclick = function (event) {
   event.preventDefault();
   if (addbtn.value == "Add Course") {
@@ -21,6 +32,7 @@ addbtn.onclick = function (event) {
     update();
   }
 };
+// add data
 function add() {
   var course = {
     name: courseName.value,
@@ -50,15 +62,50 @@ function add() {
     }
   });
 }
-
+// clear inputs
 function clear() {
   courseName.value = "";
+  courseName.classList.remove("is-valid");
+  courseName.classList.remove("is-invalid");
   courseCategory.value = "";
+  courseCategory.classList.remove("is-valid");
+  courseCategory.classList.remove("is-invalid");
   coursePrice.value = "";
+  coursePrice.classList.remove("is-valid");
+  coursePrice.classList.remove("is-invalid");
   courseDescription.value = "";
+  courseDescription.classList.remove("is-valid");
+  courseDescription.classList.remove("is-invalid");
   courseCapacity.value = "";
+  courseCapacity.classList.remove("is-valid");
+  courseCapacity.classList.remove("is-invalid");
+  if (nameAlert.classList.contains("d-block")) {
+    nameAlert.classList.replace("d-block", "d-none");
+  }
+  if (categoryAlert.classList.contains("d-block")) {
+    categoryAlert.classList.replace("d-block", "d-none");
+  }
+  if (priceAlert.classList.contains("d-block")) {
+    priceAlert.classList.replace("d-block", "d-none");
+  }
+  if (descriptionAlert.classList.contains("d-block")) {
+    descriptionAlert.classList.replace("d-block", "d-none");
+  }
+  if (capacityAlert.classList.contains("d-block")) {
+    capacityAlert.classList.replace("d-block", "d-none");
+  }
+  addbtn.setAttribute("disabled", "disabled");
+  nameFlag = 0;
+  categoryFlag = 1;
+  priceFlag = 1;
+  descriptionFlag = 1;
+  capacityFlag = 1;  
 }
-
+// clear btn
+document.getElementById("clearbtn").onclick = function () {
+  clear();
+};
+// show data in the table
 function display() {
   var data = "";
   for (var i = 0; i < courses.length; i++) {
@@ -78,7 +125,7 @@ function display() {
   }
   table.innerHTML = data;
 }
-
+// delete single
 function Delete(index) {
   Swal.fire({
     title: "Are you sure?",
@@ -97,7 +144,7 @@ function Delete(index) {
     }
   });
 }
-
+// delete all
 deleteBtn.onclick = function () {
   Swal.fire({
     title: "Are you sure?",
@@ -116,7 +163,7 @@ deleteBtn.onclick = function () {
     }
   });
 };
-
+// search function
 search.onkeyup = function () {
   var data = "";
   for (var i = 0; i < courses.length; i++) {
@@ -137,7 +184,7 @@ search.onkeyup = function () {
   }
   table.innerHTML = data;
 };
-
+// update function
 function get(index) {
   var course = courses[index];
   courseName.value = course.name;
@@ -175,3 +222,137 @@ function update() {
     }
   });
 }
+
+// validation(regex)
+// check flags status
+function validate() {
+  if (
+    nameFlag &&
+    categoryFlag &&
+    priceFlag &&
+    descriptionFlag &&
+    capacityFlag
+  ) {
+    addbtn.removeAttribute("disabled");
+  }
+}
+// name
+// first letter is capital , 3-10 letters , no numbers
+courseName.onkeyup = function () {
+  var pattern = /^[A-Z][a-z]{2,10}$/;
+  if (pattern.test(courseName.value)) {
+    nameFlag = 1;
+    if (courseName.classList.contains("is-invalid")) {
+      courseName.classList.replace("is-invalid", "is-valid");
+      nameAlert.classList.replace("d-block", "d-none");
+    } else {
+      courseName.classList.add("is-valid");
+    }
+  } else {
+    nameFlag = 0;
+    addbtn.setAttribute("disabled", "disabled");
+    nameAlert.classList.replace("d-none", "d-block");
+    if (courseName.classList.contains("is-valid")) {
+      courseName.classList.replace("is-valid", "is-invalid");
+    } else {
+      courseName.classList.add("is-invalid");
+    }
+  }
+  validate();
+};
+// category
+// first letter is capital , 3-20 letters , no numbers
+courseCategory.onkeyup = function () {
+  var pattern = /^[A-Z][a-z]{2,20}$/;
+  if (pattern.test(courseCategory.value)) {
+    categoryFlag=1;
+    if (courseCategory.classList.contains("is-invalid")) {
+      courseCategory.classList.replace("is-invalid", "is-valid");
+      categoryAlert.classList.replace("d-block", "d-none");
+    } else {
+      courseCategory.classList.add("is-valid");
+    }
+  } else {
+    categoryFlag=0;
+    addbtn.setAttribute("disabled", "disabled");
+    categoryAlert.classList.replace("d-none", "d-block");
+    if (courseCategory.classList.contains("is-valid")) {
+      courseCategory.classList.replace("is-valid", "is-invalid");
+    } else {
+      courseCategory.classList.add("is-invalid");
+    }
+  }
+  validate();
+};
+// price
+// only numbers and 3 digits
+coursePrice.onkeyup = function () {
+  var pattern = /^[0-9]{3,4}$/;
+  if (pattern.test(coursePrice.value)) {
+    priceFlag=1;
+    if (coursePrice.classList.contains("is-invalid")) {
+      coursePrice.classList.replace("is-invalid", "is-valid");
+      priceAlert.classList.replace("d-block", "d-none");
+    } else {
+      coursePrice.classList.add("is-valid");
+    }
+  } else {
+    priceFlag=0;
+    addbtn.setAttribute("disabled", "disabled");
+    priceAlert.classList.replace("d-none", "d-block");
+    if (coursePrice.classList.contains("is-valid")) {
+      coursePrice.classList.replace("is-valid", "is-invalid");
+    } else {
+      coursePrice.classList.add("is-invalid");
+    }
+  }
+  validate();
+};
+// description
+// maximum 120 character
+courseDescription.onkeyup = function () {
+  var pattern = /^.{0,120}$/;
+  if (pattern.test(courseDescription.value)) {
+    descriptionFlag=1;
+    if (courseDescription.classList.contains("is-invalid")) {
+      courseDescription.classList.replace("is-invalid", "is-valid");
+      descriptionAlert.classList.replace("d-block", "d-none");
+    } else {
+      courseDescription.classList.add("is-valid");
+    }
+  } else {
+    descriptionFlag=0;
+    addbtn.setAttribute("disabled", "disabled");
+    descriptionAlert.classList.replace("d-none", "d-block");
+    if (courseDescription.classList.contains("is-valid")) {
+      courseDescription.classList.replace("is-valid", "is-invalid");
+    } else {
+      courseDescription.classList.add("is-invalid");
+    }
+  }
+  validate();
+};
+// capacity
+// only numbers and 1-3 digits
+courseCapacity.onkeyup = function () {
+  var pattern = /^[0-9]{1,3}$/;
+  if (pattern.test(courseCapacity.value)) {
+    capacityFlag=1;
+    if (courseCapacity.classList.contains("is-invalid")) {
+      courseCapacity.classList.replace("is-invalid", "is-valid");
+      capacityAlert.classList.replace("d-block", "d-none");
+    } else {
+      courseCapacity.classList.add("is-valid");
+    }
+  } else {
+    capacityFlag=0;
+    addbtn.setAttribute("disabled", "disabled");
+    capacityAlert.classList.replace("d-none", "d-block");
+    if (courseCapacity.classList.contains("is-valid")) {
+      courseCapacity.classList.replace("is-valid", "is-invalid");
+    } else {
+      courseCapacity.classList.add("is-invalid");
+    }
+  }
+  validate();
+};
